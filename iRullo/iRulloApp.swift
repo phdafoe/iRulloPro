39485debfeee237b55861a2ec618f5af525a09af
phdafoe <phdafoe@gmail.com>
2023-10-15 13:04:58 +0200

    Initial Commit

---
+++ iRullo/iRulloApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRulloApp.swift
+//  iRullo
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iRulloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
