cf899de699330594cc41377b9208ee8a3916d927
phdafoe <phdafoe@gmail.com>
2023-10-22 15:34:51 +0200

    [WIP] - Custom insert data new task on board list

---
+++ iRullo/Views/View+AlertTextField.swift
@@ -0,0 +1,8 @@
+//
+//  View+AlertTextField.swift
+//  iRullo
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 21/10/23.
+//
+
+import Foundation
---
+++ iRullo/Views/ViewDetailCard.swift
@@ -0,0 +1,18 @@
+//
+//  ViewDetailCard.swift
+//  iRullo
+//
+//  Created by Andres Felipe Ocampo Eljaiek on 21/10/23.
+//
+
+import SwiftUI
+
+struct ViewDetailCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ViewDetailCard()
+}
